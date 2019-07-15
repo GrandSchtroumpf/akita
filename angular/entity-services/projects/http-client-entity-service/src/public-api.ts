ec187208a22b8ce8afbb025918d8caf2eac83fb0
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-07-15 21:10:22 +0200

    feat(entity-service): create angular project

---
+++ angular/entity-services/projects/http-client-entity-service/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of http-client-entity-service
+ */
+
+export * from './lib/http-client-entity-service.service';
+export * from './lib/http-client-entity-service.component';
+export * from './lib/http-client-entity-service.module';
